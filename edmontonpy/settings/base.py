dba8b1496004b0285d94222461a6fa29bd7bf7af
DeepShikha11 <58968093+DeepShikha11@users.noreply.github.com>
2020-10-01 15:38:03 +0530

    Update base.py
    changes in the INSTALLED_APPS of /settings/base.py

--- edmontonpy/settings/base.py
+++ edmontonpy/settings/base.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'Core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
